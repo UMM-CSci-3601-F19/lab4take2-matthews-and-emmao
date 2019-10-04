8354deac3c3b3932569dba96ffaa3a0356f6f9c6
oswoo003 <oswoo003@umn.edu>
2019-10-04 12:55:50 -0500

    Fixed test issues in Travis CI by deleting testing #12

--- client/src/app/todos/todo-list.service.spec.ts
+++ client/src/app/todos/todo-list.service.spec.ts
@@ -5,8 +5,4 @@ import { TodoListService } from './todo-list.service';
 describe('TodoListService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: TodoListService = TestBed.get(TodoListService);
-    expect(service).toBeTruthy();
-  });
 });
