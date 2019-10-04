20d599045c4f78ee6dc45b33793186e370aa15b2
oswoo003 <oswoo003@umn.edu>
2019-10-04 12:35:56 -0500

    Updated Todo-List Service typo #3

--- client/src/app/todos/todo-list.service.spec.ts
+++ client/src/app/todos/todo-list.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { TodoListService } from './todo-list.service';
 
-describe('UserListService', () => {
+describe('TodoListService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
